e77bcec644c31e4ef818789aeee2875c11b87b04
Jimmy Zhang <jimmy19990@live.com>
2016-01-29 19:27:16 +0800

    Update ojsettings.php

--- web/inc/ojsettings.php
+++ web/inc/ojsettings.php
@@ -1,5 +1,5 @@
 <?php
-$is_linux=1 //0=False, 1=True
+$is_linux=1; //0=False, 1=True
 date_default_timezone_set("PRC"); //Time zone settings
 $daystart = 6;  //Start hour of Day Mode
 $nightstart = 22;  //Start hour of Night Mode
