635f79e40e4afdb1c4d5c34c180e25210cec0cac
Jimmy Zhang <jimmy19990@live.com>
2016-02-07 17:58:00 +0800

    February Update Preview (0.78 beta)
    Emergent fix on news composing.
    ==========================
    1. Optimized index animation. Happy Chinese New Year!
    2. Improved admin.php redirecting experience.

--- web/ajax_getnews.php
+++ web/ajax_getnews.php
@@ -7,7 +7,7 @@ $newsid = $_POST['newsid'];
 $res=mysqli_query($con,"select title, content,time from news where news_id=$newsid");
 $row=mysqli_fetch_row($res);
 $title = $row[0];
-$content=($res && ($row)) ? str_replace('<br>', "\n", $row[1]) : '';
+$content=$row[1];
 $time = $row[2];
 $result = $title.'FuckZK1'.$content.'fUCKzk2'.$time;
 echo $result;
--- web/inc/ojsettings.php
+++ web/inc/ojsettings.php
@@ -4,7 +4,7 @@ date_default_timezone_set("PRC"); //Time zone settings
 $daystart = 6;  //Start hour of Day Mode
 $nightstart = 22;  //Start hour of Night Mode
 $oj_name = 'CWOJ';  //OJ Name
-$web_ver = '0.78b.160207-1134';
+$web_ver = '0.78b.160207-1755';
 $daemon_ver = '1.0.bsoj_original';
 $copyright = 'CWOJ Team';  //Copyright info
 $errorimg_num = 2;  //404 page img num
