09468c6603e3302f8b13e4c278849ed85bd34f10
t123yh <t123yh@outlook.com>
2016-12-17 08:24:53 +0800

    Add option to customize Gravatar CDN.

--- web/func/userinfo.php
+++ web/func/userinfo.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once __DIR__.'/../conf/ojsettings.php';
+
 //Obtain user's real IP Address.
 function get_ip(){
     if(!empty($_SERVER['HTTP_CLIENT_IP'])){
@@ -13,8 +16,7 @@ function get_ip(){
 //Obtain user's Gravatar.
 function get_gravatar($email, $s=80, $d='mm'){
     $email = md5($email); 
-    $avatar = "//secure.gravatar.com/avatar/$email?s=$s&d=$d&r=g"; 
-    //$avatar = "//sdn.geekzu.org/avatar/$email?s=$s&d=$d&r=g";
+    $avatar = constant('GRAVATAR_CDN')."/$email?s=$s&d=$d&r=g"; 
     return $avatar; 
 }
 
