a0cd095f2f1ebb4f89a392394dffbbbf7053726d
t123yh <t123yh@outlook.com>
2017-01-06 22:22:28 +0800

    Minimize CSS.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     module: {
         loaders: [
-        { test: /\.css$/, loader: 'style-loader!css-loader' },
+        { test: /\.css$/, loader: 'style-loader!css-loader?minimize' },
         { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
         {
             test: /\.js$/,
