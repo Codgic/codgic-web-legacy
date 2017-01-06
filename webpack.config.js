9377e8cc0cf93b321a143ddd15fe70466d380b1f
t123yh <t123yh@outlook.com>
2017-01-06 21:59:42 +0800

    Put vendor code into vendor.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path');
 
 module.exports = {
     entry: {
         'commons': './assets/commons.js',
     },
     performance: {
-          hints: false
+        hints: false
     },
     output: {
         path: __dirname + '/web/assets_webpack',
@@ -14,25 +14,25 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-	    {
-		test: /\.js$/,
-		exclude: /(node_modules|bower_components)/,
-		loader: 'babel-loader?presets[]=es2015'
-	    },
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'file-loader',
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-                loader: 'url-loader',
-                query: {
-                    limit: 10000,
-                    publicPath: 'assets_webpack/'
-                }
+        { test: /\.css$/, loader: 'style-loader!css-loader' },
+        { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader?presets[]=es2015'
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            loader: 'file-loader',
+        },
+        {
+            test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+            loader: 'url-loader',
+            query: {
+                limit: 10000,
+                publicPath: 'assets_webpack/'
             }
+        }
         ]
     },
     plugins: [
@@ -41,6 +41,26 @@ module.exports = {
                 warnings: false
             }
         }),
-
-    ]
-}
+        // split vendor js into its own file
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module, count) {
+                // any required modules inside node_modules are extracted to vendor
+                console.log("!!!" + module.resource + " " + (
+                        module.resource &&
+                        (/\.js|\.css|\.less$/.test(module.resource)) &&
+                        module.resource.indexOf(
+                            path.join(__dirname, 'node_modules')
+                            ) === 0
+                       ));
+                return (
+                        module.resource &&
+                        (/\.js|\.css|\.less$/.test(module.resource)) &&
+                        module.resource.indexOf(
+                            path.join(__dirname, 'node_modules')
+                            ) === 0
+                       )
+            }
+        }),
+    ],
+};
