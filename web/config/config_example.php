1e54805de732b98dc2357802d9edc6a424872d55
Tian Yunhao <t123yh@outlook.com>
2017-02-25 21:54:34 +0800

    Add config comments.

--- web/config/config_example.php
+++ web/config/config_example.php
@@ -3,17 +3,29 @@ define('OJ_NAME', 'Codgic');
 define('OJ_COPYRIGHT', '(C) 2015 - 2017 Codgic Team and Contributors');
 define('CONTACT_EMAIL', 'info@codgi.cf');
 
+/*
+* Temporary Location
+* ----------------
+* You'll need to define a temporary directory for Codgic to store a temporary file named "Codgic_postmessage.lock"
+* This acts as the mutex lock, which is necessary for message board to work.
+* Please ensure that PHP has the permission to R/W in the directory you've defined.
+*/
 define('POSTMESSAGE_LOCK', '/tmp/codgic_message.lock');
 
 define('DISPLAY_LANGUAGE', 'en_US');
+// If this option is set to `true`, all users will have to login to access the whole website.
 define('ACCESS_REQUIRES_LOGIN', false);
+// If this option is set to `true`, all newly registered users will be disabled and have to be enabled manually.
 define('REGISTER_REQUIRES_CONFIRMATION', false);
 
 define('TIME_ZONE', 'PRC');
+
+// Set the start and end for automated night mode switching.
 define('DAY_STARTS', 7);
 define('NIGHT_STARTS', 20);
 
-define('CWOJ_SCORE_MIN', 50);
+// Set the score lower limit for the CODGIC contest.
+define('CODGIC_SCORE_MIN', 50);
 
 define('MATHJAX_INLINE_BEGIN',  '[inline]');
 define('MATHJAX_INLINE_END',  '[/inline]');
@@ -34,8 +46,9 @@ define('SMTP_DISPLAY', 'Codgic');
 
 define("GRAVATAR_CDN",  '//cdn.v2ex.com/gravatar');
 
+// Please change your cookie key to a random value.
 define("COOKIE_KEY", "C0DG1C_HELLO_WORLD!");
-define("COOKIE_EXPIRE",31536000);
+define("COOKIE_EXPIRE", 31536000);
 // define("BIND_DOMAIN", '.example.com');
 
 /**
@@ -43,7 +56,7 @@ define("COOKIE_EXPIRE",31536000);
 * ----------------
 * Currently Codgic uses an RSA public key to encrypt user password.
 * Please destory the private key immediately after generating a new RSA key. 
-* Should use a hash here, What a fucking piece of shit
+* Should use a hash here, but we cannot change it now.
 */
 define("PASSWORD_PUBLIC_KEY", "
 -----BEGIN PUBLIC KEY-----
