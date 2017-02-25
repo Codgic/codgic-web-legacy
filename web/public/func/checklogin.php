d2185c0717ba3e272cccd0727e7b8068f319b63d
Tian Yunhao <t123yh@outlook.com>
2017-02-25 21:18:44 +0800

    Finished config file move.

--- web/public/func/checklogin.php
+++ web/public/func/checklogin.php
@@ -3,16 +3,14 @@ static $check_login=1;
 if(!isset($_SESSION))
     session_start();
 if(!isset($_SESSION['user'])){
-    if(!function_exists('check_cookie')) 
-        require __DIR__.'/cookie.php';
+	require_once __DIR__.'/cookie.php';
     if(!check_cookie()){
         if($require_auth){
             header("location: /login.php");
              exit();
         }
     }else{
-        if(!function_exists('login')) 
-            require __DIR__.'/userlogin.php';
+		require_once __DIR__.'/userlogin.php';
         if(TRUE===login($_SESSION['user'], TRUE))
             write_cookie(1);
     }
