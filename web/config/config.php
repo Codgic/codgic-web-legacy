c197b3e932be3ee2f183cb002b9c7b968dc35cb4
Tian Yunhao <t123yh@outlook.com>
2017-02-25 20:28:05 +0800

    Move config file.

--- web/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('MATHJAX_INLINE_BEGIN',  '[inline]');
-define('MATHJAX_INLINE_END',  '[/inline]');
-define('MATHJAX_TEX_BEGIN',  '[tex]');
-define('MATHJAX_TEX_END',  '[/tex]');
---
+++ web/config/config_example.php
@@ -0,0 +1,11 @@
+<?php
+define('MATHJAX_INLINE_BEGIN',  '[inline]');
+define('MATHJAX_INLINE_END',  '[/inline]');
+define('MATHJAX_TEX_BEGIN',  '[tex]');
+define('MATHJAX_TEX_END',  '[/tex]');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'codgic');
+define('DB_USER', 'codgic');
+define('DB_PASSWORD', 'YOURPASSWORD');
+define('DB_CHARSET', 'utf8mb4');
