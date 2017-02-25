2ca004c67a4dc7383054cb61cb17b1aa56bc106d
Tian Yunhao <t123yh@outlook.com>
2017-02-25 21:47:13 +0800

    Fix footer style.

--- web/public/inc/footer.php
+++ web/public/inc/footer.php
@@ -1,9 +1,8 @@
 <?php
 if(!isset($oj_name))
     require __DIR__.'/../conf/ojsettings.php';
-$year = date("Y");
 ?>
 <hr>
 <footer>
-    <p>&copy; <?php echo "$year $oj_copy";?></p>
+    <p><?php echo $oj_copy;?></p>
 </footer>
