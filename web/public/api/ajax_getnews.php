73ab795aa4091cfc7f87574bf43ea42c8ab2e8ee
Tian Yunhao <t123yh@outlook.com>
2017-03-18 18:41:40 +0800

    Fix bug in getnews.

--- web/public/api/ajax_getnews.php
+++ web/public/api/ajax_getnews.php
@@ -2,7 +2,7 @@
 require __DIR__.'/../inc/init.php';
 require __DIR__.'/../func/privilege.php';
 require __DIR__.'/../../src/database.php';
-require_once __DIR__.'/../src/textparser.php';
+require_once __DIR__.'/../../src/textparser.php';
 
 header('Content-Type: application/json');
 
